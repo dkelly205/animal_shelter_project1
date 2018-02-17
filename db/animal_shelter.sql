1d82a17f24b878b80652525886bc06f02d87d9ca
daniel kelly <danieljkelly91@gmail.com>
2018-02-17 01:17:01 +0000

    animal_shelter.sql created

---
+++ db/animal_shelter.sql
@@ -0,0 +1,24 @@
+DROP TABLE animals;
+DROP TABLE owners;
+
+
+CREATE TABLE animals
+(
+  id SERIAL8 primary key,
+  name VARCHAR(255) not null,
+  type VARCHAR(255),
+  breed VARCHAR(255),
+  health INT,
+  admission_date TIMESTAMP,
+  adoptable BOOLEAN
+);
+
+CREATE TABLE owners
+(
+  id SERIAL8 primary key,
+  name VARCHAR(255) not null,
+  address VARCHAR(255),
+  phone_number VARCHAR(255) not null,
+  animal_id REFERENCES animals(id) ON DELETE CASCADE
+
+);
