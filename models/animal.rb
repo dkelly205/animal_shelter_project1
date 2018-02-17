001393f56b4ff8a3c280ca6d375e3d965e1a3205
daniel kelly <danieljkelly91@gmail.com>
2018-02-17 10:17:38 +0000

    save method added to animal class

--- models/animal.rb
+++ models/animal.rb
@@ -14,4 +14,10 @@ class Animal
     @adoptable = options("adoptable")
   end
 
+  def save()
+    sql = "INSERT INTO animals (name, type, breed, health, admission_date, adoptable) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id"
+    values = [@name, @type, @breed, @health, @admission_date, @adoptable]
+    @id = SqlRunner.run(sql, values)[0]["id"].to_i()
+  end
+
 end
