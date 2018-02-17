51acc8d0c9e1e735993ab16058419aaabc0260aa
daniel kelly <danieljkelly91@gmail.com>
2018-02-17 01:10:43 +0000

    owner class created and initialise method added

---
+++ models/owner.rb
@@ -0,0 +1,13 @@
+class Owner
+
+  attr_reader :id, :name, :address, :phone_number, :pets
+
+  def initialize(options)
+    @id = options["id"].to_i
+    @name = options["name"]
+    @address = options["address"]
+    @phone_number = options["phone_number"]
+    @pets = options["pets"]
+  end
+
+end
